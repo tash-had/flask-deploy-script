8e6ad7a4bb271ce2abebd386521158a2c638b63e
Tash-had Saqif <me@tash-had.com>
2020-04-29 02:58:56 -0400

    u

--- deploy.sh
+++ deploy.sh
@@ -177,17 +177,8 @@ function create_launch_script () {
     echo ====== Starting new instance to run on port $DEPLOYMENT_PORT ========
     sudo $VM_HOME_DIR/venv/bin/gunicorn -b 0.0.0.0:$DEPLOYMENT_PORT --env APP_CONFIG=${DEPLOYMENT_ENV} --daemon ${module_name}:$PROJECT_APP_VARIABLE
 
-    new_gunicorn=\$(ps ax | grep gunicorn | grep $DEPLOYMENT_PORT)
-    
-    if [[ -z "\${new_gunicorn// }" ]]; then
-        # Retrying deployment without --daemon flag to force error log to print to stdout
-        sudo $VM_HOME_DIR/venv/bin/gunicorn -b 0.0.0.0:$DEPLOYMENT_PORT --env APP_CONFIG=${DEPLOYMENT_ENV} ${module_name}:$PROJECT_APP_VARIABLE
-        printf "***************************************************\n\t\tDeployment Failed. \n***************************************************\n"
-    else
-        echo "since we succeeded the new_gunicorn is"
-        echo $new_gunicorn
-        printf "***************************************************\n\t\tDeployment Succeeded. \n***************************************************\n"
-    fi
+    printf "***************************************************\n\t\tProblems?\nRun "sudo $VM_HOME_DIR/venv/bin/gunicorn -b 0.0.0.0:$DEPLOYMENT_PORT --env APP_CONFIG=${DEPLOYMENT_ENV} ${module_name}:$PROJECT_APP_VARIABLE for more logs.\n***************************************************\n"
+    printf "***************************************************\n\t\tDeployment Process Completed. \n***************************************************\n"
 EOF
 
     echo ====== Giving user rights to execute launch script ========
