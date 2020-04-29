e33af3837d0f99490c3978eb1ea879c591c8e8e7
Tash-had Saqif <me@tash-had.com>
2020-04-29 02:31:19 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -179,13 +179,12 @@ function create_launch_script () {
 
     new_gunicorn=\$(ps ax | grep gunicorn | grep $DEPLOYMENT_PORT)
     
-    if [ -z "\$new_gunicorn" ]; then
+    if [ -z "\${new_gunicorn// }" ]; then
         # Retrying deployment without --daemon flag to force error log to print to stdout
         sudo $VM_HOME_DIR/venv/bin/gunicorn -b 0.0.0.0:$DEPLOYMENT_PORT --env APP_CONFIG=${DEPLOYMENT_ENV} ${module_name}:$PROJECT_APP_VARIABLE
         printf "***************************************************\n\t\tDeployment Failed. \n***************************************************\n"
     else
         echo "since we succeeded the new_gunicorn is"
-        echo $new_gunicorn
         printf "***************************************************\n\t\tDeployment Succeeded. \n***************************************************\n"
     fi
 EOF
