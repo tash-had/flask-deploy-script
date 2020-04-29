f461fc21234364e5d20771caa4962b32265984e3
Tash-had Saqif <me@tash-had.com>
2020-04-29 03:02:51 -0400

    escape quotes

--- deploy.sh
+++ deploy.sh
@@ -177,7 +177,7 @@ function create_launch_script () {
     echo ====== Starting new instance to run on port $DEPLOYMENT_PORT ========
     sudo $VM_HOME_DIR/venv/bin/gunicorn -b 0.0.0.0:$DEPLOYMENT_PORT --env APP_CONFIG=${DEPLOYMENT_ENV} --daemon ${module_name}:$PROJECT_APP_VARIABLE
     printf "\n\n"
-    echo ====== PROBLEMS? RUN "$VM_HOME_DIR/venv/bin/gunicorn -b 0.0.0.0:$DEPLOYMENT_PORT ${module_name}:$PROJECT_APP_VARIABLE" FOR MORE LOGS ========
+    echo ====== PROBLEMS? RUN \"$VM_HOME_DIR/venv/bin/gunicorn -b 0.0.0.0:$DEPLOYMENT_PORT ${module_name}:$PROJECT_APP_VARIABLE\" FOR MORE LOGS ========
     printf "***************************************************\n\t\tDeployment Completed. \n***************************************************\n"
 EOF
 
